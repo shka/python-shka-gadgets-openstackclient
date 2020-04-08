47c5e5a32956743c03b112de06a1bd607da9ca02
Shintaro Katayama <shintaro.katayama@gmail.com>
2020-04-08 18:28:22 +0200

    Fix: Support 'NOT_EXIST' state in delete_images() of _KeepImages class
    Issue: #27

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ except IOError:
 
 setup(
     name='python-shka-gadgets-openstackclient',
-    version='0.5.3',
+    version='0.5.4',
     url='https://github.com/shka/python-shka-gadgets-openstackclient',
     author='Shintaro Katayama',
     author_email='shintaro.katayama@gmail.com',
